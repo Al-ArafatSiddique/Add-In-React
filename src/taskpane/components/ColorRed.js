c025fc2766b425d0b18ed8c2bce2b91b0345445b
arafat <alarafatsiddique@gmail.com>
2022-10-24 17:29:49 +0600

    working with array

--- src/taskpane/components/ColorRed.js
+++ src/taskpane/components/ColorRed.js
@@ -1,21 +1,42 @@
-import * as React from "react";
+import React from "react";
+import { useState } from "react";
+
 const ColorRed = () =>{
+  
+  const [name, setName] = useState("");
     const click = async () => {
         try {
           await Excel.run(async (context) => {
-            /**
-             * Insert your Excel code here
-             */
+
             const range = context.workbook.getSelectedRange();
+            range.load("values");
+            let valArray = range.load("values");
+            
+  
+            // range.format.fill.color = `${name}`;
+            // console.log(name);
     
-            // Read the range address
-            range.load("address");
-    
-            // Update the fill color
-            range.format.fill.color = "red";
-    
+            let range2 = context.workbook.worksheets.getItem('Sheet1');
+            let array=[];
             await context.sync();
-            console.log(`The range address was ${range.address}.`);
+            for(let i=0;i<3;i++){
+              for(let j=0; j<3;j++){
+                console.log(`${range.values[i][j]}`);
+                array.push(`${range.values[i][j]}`);
+              }
+              
+            }
+            console.log(array);
+            for(let k=1; k<array.length; k++){
+              range2.getRange(`A${k}`).values = array[k-1]
+
+            }
+            // console.log(`${range.values[1]}`);
+            // console.log(`${range.values[2]}`);
+           //let range2 = context.workbook.worksheets.getItem('Sheet1').getRange("B2:D2");
+          //range2.values= [[`${range.values[0]}`, `${range.values[1]}`,`${range.values[2]}`]]
+
+         
           });
         } catch (error) {
           console.error(error);
@@ -24,7 +45,17 @@ const ColorRed = () =>{
     return(
     <div>
        <h4>The below button colors your selected cell to Red</h4>
-    { <button onClick={click}>Color Red</button>}
+       <form onSubmit={click}>
+      <label>Enter your preferred color:
+        <input
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+    </form>
+
+    { <button onClick={click}>Color the cell</button>}
 
    </div>
     )
